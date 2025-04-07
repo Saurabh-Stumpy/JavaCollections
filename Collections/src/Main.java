de90cae6bff47e76d67878554f0669397350782c
Saurabh-Stumpy <walanjuwani@gmail.com>
2025-04-07 07:52:17 +0530

    List

---
+++ Collections/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        
+//        list.remove(0);
+
+        System.out.println(list);
+
+    }
+}
